4c2a10ceac9dfc00e90468efc3cdb01789f8bed9
Kostya Shebeko <kostya.shebeko@eon-games.com>
2026-05-04 21:18:37 +0300

    Тесты Steam UI

---
+++ locator_steam/games/games_locator.py
@@ -0,0 +1,7 @@
+game_steam = "(//*[contains(concat(' ', normalize-space(@class), ' '), ' responsive_search_name_combined ')])[1]"
+add_game_basket = "//div[@class='btn_addtocart']"
+open_basket = '(//dialog//button)[3]'
+basket = "//div[text()='Windrose']"
+crimson_desert = "//span[contains(@class, 'title') and text()='Crimson Desert']"
+basket_for_delete = "//div[text()='Crimson Desert']"
+remove_button_crimson = "//div[text()='Удалить все товары']"
